f8379a04310d1f499f5c3b403294f46f23459170
Evgeniy Didin <Evgeniy.Didin@synopsys.com>
2018-05-11 17:49:01 +0300

    Make all quake/openarena files install to image
    *Add script which downloads of openarena's pak* files
     and stores them to /root/.q3a/baseq3/ directory
    *Increase size of rootfs in defconifg
    *Add run_openarena script
    *Modify openarena controls
    
    Signed-off-by: Evgeniy Didin <Evgeniy.Didin@synopsys.com>

---
+++ board/synopsys/hsdk/openarena_download.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+wget -O openarena.zip www.openarena.ws/request.php?4 
+if [ $? -eq 0 ]
+        then
+                echo "Openarena files downloaded successfully"
+        else
+                echo "Error: Unable to download openarena zip archive" >&2
+		echo "Manually download archive from www.openarena.ws and place it in buildroot directory"
+		echo "Then run commands in board/synopsys/hsdk/openarena_download.sh below this message" 
+		exit 1
+        fi
+# Run commands below if you download zip archive manually
+unzip -o openarena.zip 
+mkdir -p output/target/root/.q3a/baseq3/
+cp openarena-0.8.8/baseoa/pak* output/target/root/.q3a/baseq3/
